320e1bc9b4c2aa808f152242ad8d9ec1fa748b66
Frances Hughes <franceslhughes@hotmail.com>
2022-12-01 11:01:11 +1300

    added header jsx and updated app.jsx

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 
 import Nav from './Nav'
-import Fruits from './Fruits'
+import Header from './Header'
 import Profile from './Profile'
 
 function App() {
   return (
     <>
+     <div className="App">
+     <Header />
       <Nav />
       <main className="flex items-center justify-center h-screen">
         <Routes>
@@ -15,8 +17,19 @@ function App() {
           <Route path="/profile" element={<Profile />} />
         </Routes>
       </main>
+      </div>
     </>
   )
 }
 
 export default App
+
+
+
+
+// <h1>This is the start page</h1>
+// <Cards />
+// <Buttons />
+// <Chats />
+// <SingleChat />
+
---
+++ client/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Header() {
+  return;
+  <div className="header">
+    <LogoButton>
+      <PersonLogo className="header_logo" fontsize="large" />
+    </LogoButton>
+    <img
+      src="https://cdn.pixabay.com/photo/2022/06/02/18/17/car-7238650_1280.png"
+      alt="carmigo logo"
+    />
+    <LogoButton>
+      <MessageLogo className="header_logo" fontSize="large" />
+    </LogoButton>
+  </div>;
+}
+
+export default Header;
