5d945f7f26defb32e843b72dbde4cd84531772d3
richard <dev.richard.so@gmail.com>
2022-12-02 12:41:11 +1300

    resolved conflicts

--- client/components/UserRoutes.jsx
+++ client/components/UserRoutes.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
+import { IfAuthenticated } from './Authenticated'
 import Footer from './Footer'
 
 function UserRoutes() {
   return (
-    <>
-      <h2>logged in</h2>
-      <Footer />
-    </>
+    <IfAuthenticated>
+      <div className="container">
+        <Logo />
+        <Routes>
+          <Route path="/" element={<SwipePage />} />
+          <Route path="/msg" element={<MessagePage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+        </Routes>
+        <Footer />
+      </div>
+    </IfAuthenticated>
   )
 }
 
-
-
 export default UserRoutes
-
