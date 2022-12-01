4174fd972953c2351b7ed4f1f2337f6c1b323b1f
Frances Hughes <franceslhughes@hotmail.com>
2022-12-01 16:14:22 +1300

    sorted out auth0, .env secret code and index.jsx

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
+import Fruits from './Fruits'
 
 import Nav from './Nav'
 import Profile from './Profile'
@@ -7,14 +8,14 @@ import Profile from './Profile'
 function App() {
   return (
     <>
-     <div className="App">
-      <Nav />
-      <main className="flex items-center justify-center h-screen">
-        <Routes>
-          <Route path="/" element={<Fruits />} />
-          <Route path="/profile" element={<Profile />} />
-        </Routes>
-      </main>
+      <div className="App">
+        <Nav />
+        <main className="flex items-center justify-center h-screen">
+          <Routes>
+            <Route path="/" element={<Fruits />} />
+            <Route path="/profile" element={<Profile />} />
+          </Routes>
+        </main>
       </div>
     </>
   )
@@ -22,12 +23,9 @@ function App() {
 
 export default App
 
-
-
 // <Header />
 // <h1>This is the start page</h1>
 // <Cards />
 // <Buttons />
 // <Chats />
 // <SingleChat />
-
