46c466b30f7cc0e025876f03f8084bf1ceded641
Frances Hughes <franceslhughes@hotmail.com>
2022-12-02 10:26:38 +1300

    3 icons all same size

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,19 +1,21 @@
-// import React from 'react';
+import React from 'react'
+import { IoCarSportSharp, TiMessages } from 'react-icons/all'
 
-// function Header() {
-//   return;
-//   <div className="header">
-//     <LogoButton>
-//       <PersonLogo className="header_logo" fontsize="large" />
-//     </LogoButton>
-//     <img
-//       src="https://cdn.pixabay.com/photo/2022/06/02/18/17/car-7238650_1280.png"
-//       alt="carmigo logo"
-//     />
-//     <LogoButton>
-//       <MessageLogo className="header_logo" fontSize="large" />
-//     </LogoButton>
-//   </div>;
-// }
+function Header() {
+  return (
+    <>
+      <div className="flex justify-center mt-96 mb-4 gap-6">
+        {/* <div className="h-96 w-96"> */}
+        <IoCarSportSharp size={70} />
+        {/* </div> */}
+        <img
+          className="object-contain h-16 w-16"
+          src="https://attachment.outlook.live.net/owa/MSA%3AFrancesLHughes%40hotmail.com/service.svc/s/GetAttachmentThumbnail?id=AQMkADAwATZiZmYAZC1mNGNjAC04NzFmLTAwAi0wMAoARgAAA8yuMD2Y7J9CirtSh%2F39APecBwAlpfnS1QZ6QYbEN1p3%2Bxg0AAACAQkAAAAlpfnS1QZ6QYbEN1p3%2Bxg0AAWTKiCVAAAAARIAEADb9MpOW8FXSo8U81s93ohx&thumbnailType=2&isc=1&token=eyJhbGciOiJSUzI1NiIsImtpZCI6IkQ4OThGN0RDMjk2ODQ1MDk1RUUwREZGQ0MzODBBOTM5NjUwNDNFNjQiLCJ0eXAiOiJKV1QiLCJ4NXQiOiIySmozM0Nsb1JRbGU0Tl84dzRDcE9XVUVQbVEifQ.eyJvcmlnaW4iOiJodHRwczovL291dGxvb2subGl2ZS5jb20iLCJ1YyI6IjUwMGVhZjIxODEyMzRlZTU5NzM0ODlmOWM1M2E4NDM3IiwidmVyIjoiRXhjaGFuZ2UuQ2FsbGJhY2suVjEiLCJhcHBjdHhzZW5kZXIiOiJPd2FEb3dubG9hZEA4NGRmOWU3Zi1lOWY2LTQwYWYtYjQzNS1hYWFhYWFhYWFhYWEiLCJpc3NyaW5nIjoiV1ciLCJhcHBjdHgiOiJ7XCJtc2V4Y2hwcm90XCI6XCJvd2FcIixcInB1aWRcIjpcIjE4OTk5NDczMTQ5Mzk2NzlcIixcInNjb3BlXCI6XCJPd2FEb3dubG9hZFwiLFwib2lkXCI6XCIwMDA2YmZmZC1mNGNjLTg3MWYtMDAwMC0wMDAwMDAwMDAwMDBcIixcInByaW1hcnlzaWRcIjpcIlMtMS0yODI3LTQ0MjM2NS00MTA3MDQ0NjM5XCJ9IiwibmJmIjoxNjY5OTI3ODY1LCJleHAiOjE2Njk5Mjg0NjUsImlzcyI6IjAwMDAwMDAyLTAwMDAtMGZmMS1jZTAwLTAwMDAwMDAwMDAwMEA4NGRmOWU3Zi1lOWY2LTQwYWYtYjQzNS1hYWFhYWFhYWFhYWEiLCJhdWQiOiIwMDAwMDAwMi0wMDAwLTBmZjEtY2UwMC0wMDAwMDAwMDAwMDAvYXR0YWNobWVudC5vdXRsb29rLmxpdmUubmV0QDg0ZGY5ZTdmLWU5ZjYtNDBhZi1iNDM1LWFhYWFhYWFhYWFhYSIsImhhcHAiOiJvd2EifQ.MjCUyc2XRs90Wqlu3B1tvAm8edC_eXE8TcNB59-7qM3DKAo_Hx5DRj9MdXdxhYYJtIYDtkh55Jv-WzuKc6hE7JTpGgPwJ_6y3Zurs_J15l6qRVBpTHBp8iAjnilTPJLchCzvJsuq424YesfiYaMDB9ZIbqTc8LcoPs62RsQkDqJMctpRY4IIKL__A5BSupqUgvzI6HsTWvUVacBT1YtyGqoYuPToAWNPMxO_fthMGfU3Ive7zQZy3bzsYDvOGWb0Fy3nUuZP7TgNr2gBj-eg6WegY0hE1hgyd2f374kLb_2X3hDfNrGab-u1kR42ttBBYe6unrTgCvUe1mt27DvcgQ&X-OWA-CANARY=eRG2D-oMEEqp3UAPomruuqDx_gbe09oY7qj6c1Zc36Jd44ZkvKxsWhM0GglKHcnnNZbKdV2liJQ.&owa=outlook.live.com&scriptVer=20221111004.17&animation=true"
+        />
+        <TiMessages size={70} />
+      </div>
+    </>
+  )
+}
 
-// export default Header;
+export default Header
--- client/components/UserRoutes.jsx
+++ client/components/UserRoutes.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
+import Header from './Header'
 
 function UserRoutes() {
-  return <h2>logged in</h2>
+  return (
+    <>
+      <h2>logged in</h2>
+      <Header />
+    </>
+  )
 }
 
 export default UserRoutes
