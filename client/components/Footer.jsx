4ce5726467398556150f7eac1c4506e57c73e516
Frances Hughes <franceslhughes@hotmail.com>
2022-12-02 16:46:52 +1300

    updated Footer.jsx and styled ProfilePage.jsx

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -4,15 +4,13 @@ import { IoCarSportSharp, TiMessages } from 'react-icons/all'
 function Footer() {
   return (
     <>
-      <div className="h-screen flex justify-center items-end">
-        <div className="flex justify-center mt-96 mb-4 gap-9 px-9 border-2 mr-14 w-min">
-          <IoCarSportSharp size={77} />
-          <img
-            className="object-contain h-14 w-14 mt-3"
-            src="https://attachment.outlook.live.net/owa/MSA%3AFrancesLHughes%40hotmail.com/service.svc/s/GetAttachmentThumbnail?id=AQMkADAwATZiZmYAZC1mNGNjAC04NzFmLTAwAi0wMAoARgAAA8yuMD2Y7J9CirtSh%2F39APecBwAlpfnS1QZ6QYbEN1p3%2Bxg0AAACAQkAAAAlpfnS1QZ6QYbEN1p3%2Bxg0AAWTKiCWAAAAARIAEACVVtMfa6LuToMK9N11GusE&thumbnailType=2&isc=1&token=eyJhbGciOiJSUzI1NiIsImtpZCI6IkQ4OThGN0RDMjk2ODQ1MDk1RUUwREZGQ0MzODBBOTM5NjUwNDNFNjQiLCJ0eXAiOiJKV1QiLCJ4NXQiOiIySmozM0Nsb1JRbGU0Tl84dzRDcE9XVUVQbVEifQ.eyJvcmlnaW4iOiJodHRwczovL291dGxvb2subGl2ZS5jb20iLCJ1YyI6IjUwMGVhZjIxODEyMzRlZTU5NzM0ODlmOWM1M2E4NDM3IiwidmVyIjoiRXhjaGFuZ2UuQ2FsbGJhY2suVjEiLCJhcHBjdHhzZW5kZXIiOiJPd2FEb3dubG9hZEA4NGRmOWU3Zi1lOWY2LTQwYWYtYjQzNS1hYWFhYWFhYWFhYWEiLCJpc3NyaW5nIjoiV1ciLCJhcHBjdHgiOiJ7XCJtc2V4Y2hwcm90XCI6XCJvd2FcIixcInB1aWRcIjpcIjE4OTk5NDczMTQ5Mzk2NzlcIixcInNjb3BlXCI6XCJPd2FEb3dubG9hZFwiLFwib2lkXCI6XCIwMDA2YmZmZC1mNGNjLTg3MWYtMDAwMC0wMDAwMDAwMDAwMDBcIixcInByaW1hcnlzaWRcIjpcIlMtMS0yODI3LTQ0MjM2NS00MTA3MDQ0NjM5XCJ9IiwibmJmIjoxNjY5OTMyMTIwLCJleHAiOjE2Njk5MzI3MjAsImlzcyI6IjAwMDAwMDAyLTAwMDAtMGZmMS1jZTAwLTAwMDAwMDAwMDAwMEA4NGRmOWU3Zi1lOWY2LTQwYWYtYjQzNS1hYWFhYWFhYWFhYWEiLCJhdWQiOiIwMDAwMDAwMi0wMDAwLTBmZjEtY2UwMC0wMDAwMDAwMDAwMDAvYXR0YWNobWVudC5vdXRsb29rLmxpdmUubmV0QDg0ZGY5ZTdmLWU5ZjYtNDBhZi1iNDM1LWFhYWFhYWFhYWFhYSIsImhhcHAiOiJvd2EifQ.HMDEidO7mXQPoZPsW_GsievkXhnYJjK7vdaHXU9H1zFtHiaDx18CxgtCY1mq0hPW99RApeKnHAN1anAq3l2t4syWmewBSfMoYJquJVE5n37TtTGFpn-EVEOlzDxaG_PKO-596JE9LAu0yh6U9kUOsly_nr4YM240kqfS-6PE4_xXdjYCHRnwOLyLptvu3r9xDkH3OtswP410PIYRfpr2bOt99y-k_Ae0JIP0JzQ54V9cJzwnmiPNs5shEA0wkQL473GNWt0U46ZJuLF0_X-5etwdTQHZusrVykHvwMOQ206eVDPAK8ixm7Cnm6MMaEVAWfiOo5iRYMlMkCiYCpgOlw&X-OWA-CANARY=vrPz9YXwbUKaOo5uSmZQ-9CLNLzn09oYAIfpc5Z5UYK9bVouDFxAo1C-lOAOoJ0bFpGqQouJY74.&owa=outlook.live.com&scriptVer=20221124007.02&animation=true"
-          />
-          <TiMessages size={77} />
-        </div>
+      <div className="h-min grid grid-cols-3 h-screen flex justify-items-center gap-9">
+        <IoCarSportSharp size={77} />
+        <img
+          className="object-contain h-14 w-14 mt-3 gap-9"
+          src="https://attachment.outlook.live.net/owa/MSA%3AFrancesLHughes%40hotmail.com/service.svc/s/GetAttachmentThumbnail?id=AQMkADAwATZiZmYAZC1mNGNjAC04NzFmLTAwAi0wMAoARgAAA8yuMD2Y7J9CirtSh%2F39APecBwAlpfnS1QZ6QYbEN1p3%2Bxg0AAACAQkAAAAlpfnS1QZ6QYbEN1p3%2Bxg0AAWTKiCWAAAAARIAEACVVtMfa6LuToMK9N11GusE&thumbnailType=2&isc=1&token=eyJhbGciOiJSUzI1NiIsImtpZCI6IkQ4OThGN0RDMjk2ODQ1MDk1RUUwREZGQ0MzODBBOTM5NjUwNDNFNjQiLCJ0eXAiOiJKV1QiLCJ4NXQiOiIySmozM0Nsb1JRbGU0Tl84dzRDcE9XVUVQbVEifQ.eyJvcmlnaW4iOiJodHRwczovL291dGxvb2subGl2ZS5jb20iLCJ1YyI6IjUwMGVhZjIxODEyMzRlZTU5NzM0ODlmOWM1M2E4NDM3IiwidmVyIjoiRXhjaGFuZ2UuQ2FsbGJhY2suVjEiLCJhcHBjdHhzZW5kZXIiOiJPd2FEb3dubG9hZEA4NGRmOWU3Zi1lOWY2LTQwYWYtYjQzNS1hYWFhYWFhYWFhYWEiLCJpc3NyaW5nIjoiV1ciLCJhcHBjdHgiOiJ7XCJtc2V4Y2hwcm90XCI6XCJvd2FcIixcInB1aWRcIjpcIjE4OTk5NDczMTQ5Mzk2NzlcIixcInNjb3BlXCI6XCJPd2FEb3dubG9hZFwiLFwib2lkXCI6XCIwMDA2YmZmZC1mNGNjLTg3MWYtMDAwMC0wMDAwMDAwMDAwMDBcIixcInByaW1hcnlzaWRcIjpcIlMtMS0yODI3LTQ0MjM2NS00MTA3MDQ0NjM5XCJ9IiwibmJmIjoxNjY5OTMyMTIwLCJleHAiOjE2Njk5MzI3MjAsImlzcyI6IjAwMDAwMDAyLTAwMDAtMGZmMS1jZTAwLTAwMDAwMDAwMDAwMEA4NGRmOWU3Zi1lOWY2LTQwYWYtYjQzNS1hYWFhYWFhYWFhYWEiLCJhdWQiOiIwMDAwMDAwMi0wMDAwLTBmZjEtY2UwMC0wMDAwMDAwMDAwMDAvYXR0YWNobWVudC5vdXRsb29rLmxpdmUubmV0QDg0ZGY5ZTdmLWU5ZjYtNDBhZi1iNDM1LWFhYWFhYWFhYWFhYSIsImhhcHAiOiJvd2EifQ.HMDEidO7mXQPoZPsW_GsievkXhnYJjK7vdaHXU9H1zFtHiaDx18CxgtCY1mq0hPW99RApeKnHAN1anAq3l2t4syWmewBSfMoYJquJVE5n37TtTGFpn-EVEOlzDxaG_PKO-596JE9LAu0yh6U9kUOsly_nr4YM240kqfS-6PE4_xXdjYCHRnwOLyLptvu3r9xDkH3OtswP410PIYRfpr2bOt99y-k_Ae0JIP0JzQ54V9cJzwnmiPNs5shEA0wkQL473GNWt0U46ZJuLF0_X-5etwdTQHZusrVykHvwMOQ206eVDPAK8ixm7Cnm6MMaEVAWfiOo5iRYMlMkCiYCpgOlw&X-OWA-CANARY=vrPz9YXwbUKaOo5uSmZQ-9CLNLzn09oYAIfpc5Z5UYK9bVouDFxAo1C-lOAOoJ0bFpGqQouJY74.&owa=outlook.live.com&scriptVer=20221124007.02&animation=true"
+        />
+        <TiMessages size={77} />
       </div>
     </>
   )
