ab9352f7039717f0d0b0def47065524472c8c828
charanjot <charanjotsingh23@gmail.com>
2022-12-02 14:51:01 +1300

    logo.jsx completed

--- client/components/SwipePage.jsx
+++ client/components/SwipePage.jsx
@@ -15,6 +15,10 @@ function SwipePage() {
       name: 'Monica Hall',
       url: '../../server/public/images/3.jpeg',
     },
+    {
+      name: 'CJ',
+      url: '../../server/public/images/4.jpg',
+    },
   ]
   const characters = db
   const [lastDirection, setLastDirection] = useState()
@@ -30,7 +34,10 @@ function SwipePage() {
 
   return (
     <>
-      <div className="w-11/12 h-5/6 justify-center items-center overflow-hidden border-2 border-solid border-red-400 ">
+      <div
+        className="w-11/12 h-4/6 justify-center items-center overflow-hidden border-2 border-solid border-red-400 bg-blue-500
+      "
+      >
         {characters.map((character) => (
           <TinderCard
             className="absolute w-full h-5/6 overflow-hidden"
