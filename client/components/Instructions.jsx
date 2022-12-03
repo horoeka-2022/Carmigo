e66ff77c77fbb971f8f19551f5841ab57072cb1f
Frances Hughes <franceslhughes@hotmail.com>
2022-12-03 17:24:15 +1300

    footer icons now clickable - take to route

--- client/components/Instructions.jsx
+++ client/components/Instructions.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Instructions() {
   return (
@@ -17,11 +18,13 @@ function Instructions() {
       <div className="text-center text-xl text-black decoration-solid">
         <p>Here's everything you need to know</p>
       </div>
-      <div className="h-min grid grid-cols-1 justify-items-center">
-        <button className="bg-blue-400 border-2 border-blue-300 text-white rounded-full decoration-solid flex justify-self-center mt-4 mb-4 gap-9 px-9 border-2 w-min">
-          Start Tutorial
-        </button>
-      </div>
+      <Link to="/">
+        <div className="h-min grid grid-cols-1 justify-items-center">
+          <button className="bg-blue-400 border-2 border-blue-300 text-white rounded-full decoration-solid flex justify-self-center mt-4 mb-4 gap-9 px-9 border-2 w-min">
+            Start Tutorial
+          </button>
+        </div>
+      </Link>
       <div className="h-36 bg-blue-200"></div>
     </>
   )
