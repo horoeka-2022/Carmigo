a56c87898090738cdb525d03d5c552db592c3cb3
charanjot <charanjotsingh23@gmail.com>
2022-12-07 11:58:11 +1300

    push to start button on demo page working

--- client/components/Demo.jsx
+++ client/components/Demo.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react'
 import TinderCard from 'react-tinder-card'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 function Demo() {
+  const navigate = useNavigate()
   const db = [
     {
       name: '',
@@ -41,14 +42,21 @@ function Demo() {
     console.log(name + ' left the screen!')
   }
 
+  function handleClick() {
+    navigate('/swipe')
+  }
+
   return (
     <>
-      <div className="w-[80vw] ">
-        <Link to="/swipe">
-          <button className="border-solid border-2 px-6 py-1 border-white rounded-3xl text-center text-xl bg-gradient-to-r from-sky-400 to-indigo-400 ">
-            Start Exploring for your new CarMigo!
-          </button>
-        </Link>
+      <div onClick={() => handleClick()}>
+        <div className="flex justify-center items-center h-60 w-60 bg-gray-900  border-4 border-solid border-slate-200 rounded-full">
+          <div className="flex flex-col justify-center items-center w-48 h-48 bg-gray-800 border-4 border-solid border-green-400 rounded-full">
+            <div className=" h-3 w-7 rounded-lg bg-green-400 mb-8"></div>
+            <div className=" text-white text-3xl font-bold text-center">
+              PUSH TO START
+            </div>
+          </div>
+        </div>
       </div>
       {characters.map((character) => (
         <TinderCard
@@ -70,3 +78,35 @@ function Demo() {
 }
 
 export default Demo
+
+//   return (
+//     <>
+//       <div className="">
+//         <div className="relative w-60 h-60 bg-gray-900 rounded-full border-4 border-solid border-slate-200">
+//           <div className="absolute top-1/6 left-1/6 w-48 h-48 bg-gray-800 rounded-full border-4 border-solid border-green-400 items-center justify-center"></div>
+//           <div className="absolute top-1/4 left-1/4 h-3 w-7 rounded-lg bg-green-400"></div>
+//           <div className="absolute top-1/4 left-1/4 text-white text-3xl font-bold ">
+//             PUSH TO START
+//           </div>
+//         </div>
+//       </div>
+//       {characters.map((character) => (
+//         <TinderCard
+//           className="flex flex-col absolute overflow-hidden"
+//           key={character.name}
+//           onSwipe={(dir) => swiped(dir, character.name)}
+//           onCardLeftScreen={() => outOfFrame(character.name)}
+//         >
+//           <div
+//             style={{ backgroundImage: 'url(' + character.url + ')' }}
+//             className="flex relative w-screen h-[78vh] justify-center bg-cover bg-center rounded-3xl"
+//           >
+//             <h3>{character.name}</h3>
+//           </div>
+//         </TinderCard>
+//       ))}
+//     </>
+//   )
+// }
+
+// export default Demo
